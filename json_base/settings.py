9d0ed5e3fd5f982cfd5b6f90a6f841c907a005e7
gorgick <gorgick2014@yandex.by>
2025-05-26 14:39:11 +0300

    postgres

--- json_base/settings.py
+++ json_base/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'json_base.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'json_base',
+        'USER': 'user_base',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
