5832e4b25811ef6eee9f689b71e5de632e2642b1
gorgick <gorgick2014@yandex.by>
2025-05-30 10:31:26 +0300

    admin for download csv, rebuild our csv

--- js_on/admin.py
+++ js_on/admin.py
@@ -1,3 +1,40 @@
-from django.contrib import admin
+from csv import DictReader
 
-# Register your models here.
+from django.contrib import admin
+from django.http import HttpRequest, HttpResponse
+from io import TextIOWrapper
+from django.shortcuts import render, redirect
+from django.urls import path
+
+from .models import DestAnime
+from .forms import CSVImportForm
+
+
+@admin.register(DestAnime)
+class AnimeAdmin(admin.ModelAdmin):
+    change_list_template = 'js_on/anime_changelist.html'
+    list_display = ("data",)
+
+    def import_csv(self, request: HttpRequest) -> HttpResponse:
+        if request.method == "GET":
+            form = CSVImportForm()
+            context = {
+                'form': form
+            }
+            return render(request, 'admin/csv_form.html', context)
+        form = CSVImportForm(request.POST, request.FILES)
+        if not form.is_valid():
+            context = {
+                'form': form
+            }
+            return render(request, 'admin/csv_form.html', context, status=400)
+
+
+
+
+    def get_urls(self):
+        urls = super().get_urls()
+        new_urls = [
+            path('import-products-csv', self.import_csv, name='import_products_csv'),
+        ]
+        return new_urls + urls
