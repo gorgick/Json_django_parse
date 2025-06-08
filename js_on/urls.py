f5e3610392026473312dfec9f59f82f5365ca99d
gorgick <gorgick2014@yandex.by>
2025-06-08 12:33:49 +0300

    db.cursor Insert

--- js_on/urls.py
+++ js_on/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from js_on.views import index
+from js_on.views import index, create_data
 
 urlpatterns = [
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('create_data', create_data, name='create_data')
 ]
--- js_on/views.py
+++ js_on/views.py
@@ -2,6 +2,26 @@ from django.shortcuts import render
 from django.db import connection
 
 
+def create_data(request):
+    with connection.cursor() as cursor:
+        cursor.execute("""INSERT INTO js_on_DestAnime VALUES
+        ('50',
+            '{"genre": ["романтика", "фантастика"],
+            "studio": "Arvo Animation",
+            "director": "Ёсиаки Ивасаки",
+            "amount_series": 10}'
+        ),
+        ('51',
+            '{"genre": ["экшен", "фантастика"],
+            "studio": "MAPPA",
+            "director": "Норихиро Наганума",
+            "amount_series": 25}'
+        );""")
+        row = cursor.fetchall()
+    print(row)
+    return render(request, 'js_on/index.html', {'row': row})
+
+
 def index(request):
     with connection.cursor() as cursor:
         cursor.execute("SELECT * FROM js_on_DestAnime")
