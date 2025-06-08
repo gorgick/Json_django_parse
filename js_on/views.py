2bf4279513b4b5e686548ea7a14cd06fa6e31fe4
gorgick <gorgick2014@yandex.by>
2025-06-08 12:18:44 +0300

    db.cursor select

--- js_on/views.py
+++ js_on/views.py
@@ -4,6 +4,7 @@ from django.db import connection
 
 def index(request):
     with connection.cursor() as cursor:
+        cursor.execute("SELECT * FROM js_on_DestAnime")
         row = cursor.fetchall()
     print(row)
     return render(request, 'js_on/index.html', {'row': row})
